907aed293060cda617b14a351be3ddca4788173a
gamegrd <gamegrd@outlook.com>
2017-06-22 15:29:35 +0800

    rename

--- djangoauth.go
+++ djangoauth.go
@@ -1,4 +1,4 @@
-package auth
+package djangoauth
 /*
 #############################################
 # File Name: djangopwd.go
--- djangoauth_test.go
+++ djangoauth_test.go
@@ -1,4 +1,4 @@
-package auth
+package djangoauth
 
 import (
     "testing"
